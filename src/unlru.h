018e90f5bdb18c31b4f9020a90aa7c200083b974
prejrdev <prejrdev@gmail.com>
2019-06-27 05:49:58 -0400

    Create unlru.h

---
+++ src/unlru.h
@@ -0,0 +1,17 @@
+//
+// Created by <prejrdev@gmail.com>
+//
+
+#ifndef UNNECESSARILY_NATIVE_LRU_UNLRU_H
+#define UNNECESSARILY_NATIVE_LRU_UNLRU_H
+
+#include <v8.h>
+#include <node.h>
+#include <node_object_wrap.h>
+
+#include "build_def.h"
+#include "debug_utils.h"
+
+#include "v8_exceptions.h"
+
+#endif //UNNECESSARILY_NATIVE_LRU_UNLRU_H
